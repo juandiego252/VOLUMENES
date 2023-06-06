c9c2aee728e2610f4458b112212f29e0efcaffdf
Juan Falconi <juan.falconi@epn.edu.ec>
2023-06-06 10:06:55 -0500

    metodos

--- src/Figuras3D.java
+++ src/Figuras3D.java
@@ -1,2 +1,77 @@
 public class Figuras3D {
+    private float lado;
+    private float altura;
+    private float base;
+    private float radio;
+    private double pi= 3.1416;
+
+    public Figuras3D(float lado, float altura, float base, float radio) {
+        this.lado = lado;
+        this.altura = altura;
+        this.base = base;
+        this.radio = radio;
+    }
+
+    public Figuras3D(float altura, float base) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+    //setters and getters publicos el resto privado
+
+
+    public Figuras3D(float lado) {
+        this.lado = lado;
+    }
+
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float volumenCubo(int lado){
+        return this.lado*this.lado*this.lado;
+    }
+
+    public float volumenPiramide(float base, float altura){
+        return ((1/3)*this.altura*this.base);
+    }
+
+    public double voloumenEsfera(float radio){
+        return ((4/3)*pi*this.radio*this.radio*this.radio);
+
+    }
+
+    public double volumenCilindo(float radio, float altura){
+        return pi*this.radio*this.radio*altura;
+    }
+
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,27 @@
 public class main {
+
+    public static void main(String[] args) {
+
+        Figuras3D cubo = new Figuras3D(5);
+        Figuras3D piramide = new Figuras3D(3);
+        Figuras3D esfera  = new Figuras3D(6);
+        Figuras3D cilindro = new Figuras3D(7);
+
+        cubo.setLado(5);
+        System.out.println( cubo.volumenCubo(4));
+
+        piramide.setBase(2);
+        piramide.setAltura(5);
+        System.out.println(piramide.volumenPiramide(2,5));
+
+        esfera.setRadio(2);
+        System.out.println(esfera.voloumenEsfera(5));
+
+        cilindro.setRadio(5);
+        cilindro.setAltura(7);
+        System.out.println(cilindro.volumenCilindo(6,3));
+    }
+
+
+
 }
